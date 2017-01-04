52e535e9c8fb4aef4f1460afae1fdb88ae50f37b
Satish Kumar <kumarnotifications@gmail.com>
2017-01-04 00:13:42 -0600

    Add acceptance test to display entries destined for the dashboard. The test will currently fail because the app does not yet have the functionality.

--- tests/acceptance/dashboard-test.js
+++ tests/acceptance/dashboard-test.js
@@ -9,3 +9,10 @@ test('should redirect to dashboard route', function (assert) {
     assert.equal(currentURL(), '/dashboard', 'should redirect automatically');
   });
 });
+
+test('should list entries meant to be listed on the dashboard', function (assert) {
+  visit('/');
+  andThen(function () {
+    assert.equal(find('.entry').length, 1, 'should see 1 entry');
+  });
+});
